a5a645c6849effd444a5d165034f6e0abfd4f3ab
Dustin Fox <dustinfox1996@gmail.com>
2021-03-08 15:15:31 -0500

    more edits

---
+++ StatementsTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StatementsTutorial {
+    class Program {
+        static void Main(string[] args) {
+
+
+            ////Console.WriteLine("Enter a number");
+            ////var aNbr = Console.ReadLine();
+            ////var nbr = Convert.ToInt32(aNbr);
+            ////if(nbr % 2 == 0) {
+            ////    Console.WriteLine($"The number {nbr} is even");
+
+
+            ////} else {
+            ////    Console.WriteLine($"The number {nbr} is odd");
+            //} //need a lot of help with this one with becca
+            var ints = new[] {77, 32, 94, 46, 28 };
+            for (var idx =0; idx < 5; idx++) {
+                Console.WriteLine($"idx is {idx} and ints[idx] is {ints[idx]}");
+            }
+
+        }
+    }
+}
